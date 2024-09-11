74c4a45560899988b2b36cd60d23175a4fb0b2ac
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2024-09-11 21:47:16 +0000

    transitions added

---
+++ components/MobileNav.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+usePathname
+
+const MobileNav = () => {
+  return (
+    <nav>
+      
+    </nav>
+  )
+}
+
+export default MobileNav
