f308a8b444f3db5de373730093d07caa62f53785
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2024-12-02 22:13:14 +0000

    resume

--- app/page.jsx
+++ app/page.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             </p>
             {/* buttons and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-4">
-              <a href="/Resume-Fynn.pdf" download>
+              <a href="/Resume-Main.pdf" download>
                 <Button
                   variant="outline"
                   size="lg"
