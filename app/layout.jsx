442e1f4d2e08aa99435586034ee99186ee5d5676
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2024-12-03 10:11:23 +0000

    ga analysis

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
-
+import Script from "next/script";
 
 export const metadata = {
   title: "My Portfolio",
@@ -12,12 +12,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GAID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());   
+          gtag('config', '${process.env.GAID}');`,
+          }}
+        />
+      </head>
       <body>
-        <Header/>
-        <StairTransition/>
-        <PageTransition>
-       {children}
-       </PageTransition>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
