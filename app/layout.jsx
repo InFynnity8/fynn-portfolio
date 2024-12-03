190920a900490a6100d85187788065d0da61c1eb
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2024-12-03 11:10:03 +0000

    script

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import Script from "next/script";
 
 export const metadata = {
   title: "My Portfolio",
@@ -12,20 +13,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
+        <Script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GAID}`}
+          strategy="afterInteractive"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());   
-          gtag('config', '${process.env.NEXT_PUBLIC_GAID}');`,
-          }}
-        />
-        </head>
+          gtag('config', '${process.env.NEXT_PUBLIC_GAID}');`}
+        </Script>
+      </head>
       <body>
         <Header />
         <StairTransition />
